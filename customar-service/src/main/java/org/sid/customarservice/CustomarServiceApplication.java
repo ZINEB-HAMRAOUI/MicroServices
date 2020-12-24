23d8605999e6b24147ec82ee1bd9ed0c3ec6409a
Zineb HAMRAOUI <zineb.hamraouiii@gmail.com>
2020-12-24 15:19:24 +0100

    customer_service

---
+++ customar-service/src/main/java/org/sid/customarservice/CustomarServiceApplication.java
@@ -0,0 +1,13 @@
+package org.sid.customarservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomarServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomarServiceApplication.class, args);
+	}
+
+}
