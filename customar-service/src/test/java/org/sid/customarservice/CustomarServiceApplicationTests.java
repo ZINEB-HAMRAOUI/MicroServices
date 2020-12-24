47448bb6dbc9b581e7b2b16c3aa77dbf8b311d4a
ZINEB-HAMRAOUI <62762631+ZINEB-HAMRAOUI@users.noreply.github.com>
2020-12-24 16:29:25 +0100

    Add files via upload

---
+++ customar-service/src/test/java/org/sid/customarservice/CustomarServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.sid.customarservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomarServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
